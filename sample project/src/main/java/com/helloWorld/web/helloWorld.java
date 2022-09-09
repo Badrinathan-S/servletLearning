daa12095c17ceda597951145fa41e2c033d95109
Badrinathan-S <badrinathan3@gmail.com>
2022-09-09 13:21:47 +0530

    Servlet hello world

---
+++ sample project/src/main/java/com/helloWorld/web/helloWorld.java
@@ -0,0 +1,15 @@
+package com.helloWorld.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+
+public class helloWorld extends HttpServlet {
+
+	public void service(ServletRequest req, ServletResponse res) throws IOException {
+		res.setContentType("text/html");
+		res.getWriter().print("Hello World!ZXZ!");
+	}
+}
